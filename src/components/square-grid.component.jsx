c29826a4068838a6a98b1de3889b8c25a25b14de
Keenan Murphy <kmurphydev@gmail.com>
2021-11-26 15:21:55 -0500

    added placeholder square grid component

---
+++ src/components/square-grid.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const SquareGrid = () =>{
+    return (
+        <div>
+            This will be a grid of squares!
+
+            </div>
+
+    );
+}
+
+export default SquareGrid;
