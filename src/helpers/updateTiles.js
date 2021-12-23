bfcf21f2406d5d5d924a3051e87abd8b0b6fc64e
Keenan Murphy <kmurphydev@gmail.com>
2021-12-23 17:23:29 -0500

    fixed updateTiles logic and made code more performant

--- src/helpers/updateTiles.js
+++ src/helpers/updateTiles.js
@@ -1,4 +1,4 @@
-
+import * as TILES from '../constants/tiles.constants.js';
 
 // updateTiles: given oldTiles and board settings, simulates the next frame of game of life
 // IMPORTANT: updateTiles must return oldTiles if none of the values change, to allow for shallow array comparison
@@ -14,17 +14,21 @@ export const updateTiles = (oldTiles, boardSettings) => {
     const tiles = new Array(gridHeight);
     for (var i = 0; i < gridHeight; i++) {
         tiles[i] = new Array(gridWidth);
-    };
-
-    for (var i = 0; i < gridHeight; i++) {
         for (var j = 0; j < gridWidth; j++) {
             const sum = sumNeighbors(i, j, oldTiles, boardSettings);
-            tiles[i][j] = sum === 3 || (sum === 4) * oldTiles[i][j];
-            if (!changed && tiles[i][j] !== oldTiles[i][j]) changed = true;
-        }
+            const oldTile = oldTiles[i][j];
+            if (sum === 4) tiles[i][j] = oldTile;
+            else {
+                const newTile = (sum === 3) * TILES.STATES.ALIVE;
+                tiles[i][j] = newTile;
+                if (!changed && newTile !== oldTile) changed = true;
+
+            }
+        };
     };
 
     if (!changed) {
+        console.log('unchanged')
         return oldTiles;
     }
     else {
@@ -38,6 +42,24 @@ const sumNeighbors = (row, col, oldTiles, boardSettings) => {
     const { gridWidth, gridHeight } = boardSettings;
 
     let sum = 0;
+
+    // let minRow = row - 1;
+    // if (minRow < 0) minRow = gridHeight - 1;
+    // let maxRow = row + 1;
+    // if (maxRow > gridHeight - 1) maxRow = 0;
+    // let minCol = col - 1;
+    // if (minCol < 0) minCol = gridWidth - 1;
+    // let maxCol = col + 1;
+    // if (maxCol > gridWidth - 1) maxCol = 0;
+
+    // sum += oldTiles[minRow][minCol];
+    // sum += oldTiles[minRow][col];
+    // sum += oldTiles[minRow][maxCol];
+    // sum+= oldTiles[row][minCol];
+    // sum += oldTiles[row][col];
+    // sum+= oldTiles[row][maxCol];
+    // sum+= oldTiles[maxRow][minCol];
+    // sum+= oldTiles[maxRow][minCol];
 
     let rows = new Array(3);
     let cols = new Array(3);
